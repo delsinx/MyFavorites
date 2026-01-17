d516c077959d449f4d90a5b0c6ea4e0ad9de4384
Delson Antônio Andrade Filho <delson_filho@protonmail.com>
2026-01-17 11:13:57 -0300

    Merge remote changes

---
+++ src/main/java/com/example/demo/MyFavoritesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavoritesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavoritesApplication.class, args);
+	}
+
+}
