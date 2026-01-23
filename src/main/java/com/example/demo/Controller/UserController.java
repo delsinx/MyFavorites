abf294561537c0bfa6940c7482197c3011763500
Delson Antônio Andrade Filho <delson_filho@protonmail.com>
2026-01-23 11:39:05 -0300

    .

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -4,7 +4,6 @@ package com.example.demo.Controller;
 import com.example.demo.DTO.UserDTO;
 import com.example.demo.Model.UserModel;
 import com.example.demo.Service.UserService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +16,7 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -24,19 +24,19 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
         UserDTO user = userService.registerUser(userDTO);
-        if(user != null){
-           return  ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
-        }else{
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User registration failed");
         }
     }
 
     @GetMapping("/{id}")
-    public UserDTO showUserByID(@PathVariable Long id){
-       return  userService.showUserById(id);
+    public UserDTO showUserByID(@PathVariable Long id) {
+        return userService.showUserById(id);
     }
 
-    @GetMapping ("/allusers")
+    @GetMapping("/allusers")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         List<UserDTO> users = userService.getAllUsers();
 
@@ -49,18 +49,18 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteUserByID(@PathVariable Long id){
-       UserDTO deleteUser = userService.deleteUserById(id);
-       if(deleteUser != null){
-           return ResponseEntity.status(HttpStatus.NO_CONTENT).body("User deleted successfully");
-       }else{
-           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
-       }
+    public ResponseEntity<String> deleteUserByID(@PathVariable Long id) {
+        UserDTO deleteUser = userService.deleteUserById(id);
+        if (deleteUser != null) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("User deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
     }
 
     @PutMapping("/update")
-    public void updateUserInfo( @RequestBody UserModel userModel) {}
-
+    public void updateUserInfo(@RequestBody UserModel userModel) {
+    }
 
 
 }
